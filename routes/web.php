e103d7c2f4bea5917e35621d200337fd2dfc3ce9
liedsonc <liedsonc@icloud.com>
2026-09-13 01:20:26 +0100

    feat: create test controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
+Route::resource('test', TestController::class);
